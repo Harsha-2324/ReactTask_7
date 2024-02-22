068d126a54bedc126cbd941a62e1bb03d4177b27
Harsha-2324 <rharshva@gmail.com>
2024-02-22 23:46:54 +0530

    formik commit

---
+++ src/Home.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <>
+      <div className="container">
+        <div className="row ">
+          <div className="heading border border-warning text-warning p-5 mt-3 mb-3 rounded-4 " style={{ backgroundColor: "#2f3d46" }}>
+            <h1 className="text-center mt-3 ">
+              Welcome to{" "}
+              <span style={{ color: "#f9bc98" }}>
+                <i>Harshavardhan Rajinikanth Library</i>
+              </span>{" "}
+              Mangement System
+            </h1>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Home
